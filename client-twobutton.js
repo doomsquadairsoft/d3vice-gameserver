6d8e24e5fc20ab095873eb5706a296028223afc7
Chris Grimmett <chris@grimtech.net>
2016-11-20 15:13:18 -0800

    add concept for auto discovering primus

--- client-twobutton.js
+++ client-twobutton.js
@@ -4,7 +4,7 @@
 var Input = require('./lib/mock-input');
 var Discoverer = require('./lib/discoverer');
 var Promise = require('bluebird');
-var Connection = require('./lib/socket-client');
+var Socket = require('./lib/socket-client');
 
 
 
@@ -29,6 +29,15 @@ Adapter.prototype.connect = function connect(input, socket) {
     self.socket = socket;
 }
 
+
+Adapter.prototype.setSocket = function setSocket(socket) {
+    self.socket = socket;
+}
+
+/**
+ * send data over the websocket
+ * if no websocket connection is active, cache the latest button pressed
+ */
 Adapter.prototype.send = function send(button) {
     if (typeof button === 'undefined')
 	throw new Error('first param sent to send() must be a button name');
@@ -48,29 +57,64 @@ var input = new Input().begin();
 
 
 
-// greets https://stackoverflow.com/a/29933215
-var waitForDiscoveryEvent = function waitForDiscoveryEvent(type) {
-    return new Promise(function(resolve) {
-	disco.on(type, resolve);
-    });
-}
 
 
 
 // poll the discovery module every 1s until we get a list of discovered servers
 // use the list of discovered servers to find the suitable D3VICE server
-var pollDiscoTimer = setInterval(function() {
+var pollDiscoTimer = setTimeout(function() {
+
     var discoList = disco.getDiscovered();
     
-    return new Promise.mapSeries(discoList, function(server) {
+    var connectionList = new Promise.mapSeries(discoList, function(server) {
 	// for each server in the list, return a promise of a connection.
 	// these fulfilled/rejected promises will be used to deterimine
 	// which connection to use for the game.
-	return new Connection(server.adddresses[0], server.port);
+	if (typeof server.addresses === 'undefined')
+	    throw new Error('server found by discoverer has no addresses. '+server);
+	return new Socket(server.addresses[0], server.port)
+	    .connect()
+	    .catch(function(err) {
+		console.log(err);
+	    })
+    })
+    
+    // go through the connections and filter out any that had connection issues
+    .filter(function(primus) {
+	
+	if (typeof primus === 'undefined')
+	    return false;
+	
+	if (typeof primus.id === 'undefined')
+	    return false
+
+	return true
+	
+    })
+    
+    
+    .then(function(p) {
+	console.log(p.length);
+	return p;
+    })
+
+    .mapSeries(function(primus) {
+	// got valid primus objects
+
+	//console.log(primus);
+	primus.on('data', function(d) {
+	    primus.id(function(id) {
+		console.log('%s got data', id);
+		console.log(d);		
+	    });
+	});
+
+
     })
 
 
-}, 1000);
+
+}, 2000);
 
 
 
@@ -78,40 +122,28 @@ var pollDiscoTimer = setInterval(function() {
 
 
 
-var waitForButton = function waitForButton(buttonName) {
-    return new Promise(function(resolve) {
-	input.on(buttonName, resolve);
-    });
-};
 
 
 
-   
 
 
-waitForButton('red')
-    .then(function() {
+
+input.on('button', function(b) {
+
+    if (b === 'red')
 	adapter.send('red')
-    });
 
-waitForButton('green')
-    .then(function() {
+    if (b === 'green')
 	adapter.send('green');
-    });
 
-waitForButton('start')
-    .then(function() {
+    if (b === 'start')
 	adapter.send('start');
-    });
 
-waitForButton('stop')
-    .then(function() {
+    if (b === 'stop')
 	adapter.send('stop');
-    });
 
-waitForButton('clear')
-    .then(function() {
+    if (b === 'clear')
 	adapter.send('clear');
-    });
 
+});
 
